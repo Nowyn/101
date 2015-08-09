06011d0da808279e5f58980147a67a94cbcc1fa6
Elena D. <danilova.elena.v@gmail.com>
2015-08-09 16:48:55 +0500

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -1,4 +1,6 @@
-//In this problem we need to find the sum of all the multiples of 3 or 5 below 1000.
+//URL: http://projecteuler.net/problem=1
+//task: to find the sum of all the multiples of 3 or 5 below 1000.
+
 var sum = 0;
 for (var i = 1; i < 1000; i++ ) {
 	if ( i % 3 === 0 || i % 5 === 0 ) {
