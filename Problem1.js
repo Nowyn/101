15db987427d0f23d84eb65ffaa17fb1d889e6a95
Elena D. <danilova.elena.v@gmail.com>
2015-08-09 14:38:04 +0500

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -1,14 +1,9 @@
 //In this problem we need to find the sum of all the multiples of 3 or 5 below 1000.
-var b = 0; //we will store the sum of all the multiples of 15 here
-var c = 0; //we will store the sum of all the multiples of 3 here
-var d = 0; // we will tore the sum of all the multiples of 5 here
-for (var i = 1; i<1000; i++) {
-    if(i % 15 === 0) {
-      b += i;
-    } else if(i % 3 ===0) {
-      c += i;
-    } else (i % 5 === 0) {
-      d += i;
-    }
+var sum = 0;
+for (var i = 1; i < 1000; i++ ) {
+	if ( i % 3 === 0 || i % 5 === 0 ) {
+		sum += i;
+	}
 }
-console.log(b+c+d) //here we will find the sum
+
+console.log(sum);	
