33c67bcfce488574f614176a884e992098c9ec5a
Elena D. <danilova.elena.v@gmail.com>
2015-08-12 18:38:46 +0500

    Update Problem3.js

--- Problem3.js
+++ Problem3.js
@@ -19,5 +19,5 @@ for (var i = 0; i < result.length; i++) { //now we are searching for the largest
         result[0] = result[i];
     }
 }
-f(600851475143); //so we call a function with our number to get an arrey of prome factors for this number, we get => [ 71, 839, 1471, 6857 ]
+f(600851475143); //so we call a function with our number to get an array of prome factors for this number, we get => [ 71, 839, 1471, 6857 ]
 console.log(result[0]); //and then search for the largest prime factor, we get 6857
