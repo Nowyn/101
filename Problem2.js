58ee82d6b52e395814310732c25fed570def71bc
Elena D. <danilova.elena.v@gmail.com>
2015-08-12 20:29:21 +0500

    Update Problem2.js

--- Problem2.js
+++ Problem2.js
@@ -1,19 +1,19 @@
 //URL: http://projecteuler.net/problem=2
-//task: to find the sum of the even-valued terms of the Fibonacci sequence.
+//task: to find the sum of the even-valued terms of the Fibonacci sequence below 4.000.000.
 
-var a = 1;
-var b = 2;
+var a = 1; //we declare the 1st term of the Fibonacci sequence
+var b = 2; //we declare the 2nd term of the Fibonacci sequence
 
-var sum = 0;
+var sum = 0; //initial sum, just to declare a variable 'sum'
 
-while (a < 4000000) {
-     if (a % 2 === 0) {
-          sum += a;
+while (a < 4000000) { //we declare the limit
+     if (a % 2 === 0) { //check if the term is even
+          sum += a; //if it is, we add it to the sum
      }
-          var c = a + b;
-          a = b;
-          b = c;
+          var c = a + b; //then we declare the 3rd term of the Fibonacci sequence which equals to the sum of two previous terms
+          a = b; //then among the 3 next terms, a (1st term) becomes b (2nd term)... 
+          b = c; //...and b (2nd term) becomes c (3rd term)
 
 }
 
-console.log(sum) //4613732
+console.log(sum) //sum = 4613732
