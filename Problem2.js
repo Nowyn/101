9f3df08e3c6ff4a44aa922549b44a9559daf3a2e
Elena D. <danilova.elena.v@gmail.com>
2015-08-12 20:31:26 +0500

    Update Problem2.js

--- Problem2.js
+++ Problem2.js
@@ -10,10 +10,14 @@ while (a < 4000000) { //we declare the limit
      if (a % 2 === 0) { //check if the term is even
           sum += a; //if it is, we add it to the sum
      }
-          var c = a + b; //then we declare the 3rd term of the Fibonacci sequence which equals to the sum of two previous terms
+          /*
+          then we check every 3 terms of the F.c., 
+          for this we declare the 3rd term of the Fibonacci sequence 
+          which equals to the sum of two previous terms
+          */
+          var c = a + b; 
           a = b; //then among the 3 next terms, a (1st term) becomes b (2nd term)... 
           b = c; //...and b (2nd term) becomes c (3rd term)
-
 }
 
 console.log(sum) //sum = 4613732
